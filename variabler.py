4522e7dc8b99b4acc1a6a0f1c5fc5ef589346c22
Emmanuel karlsson <emmanuel.karlsson@elev.ga.ntig.se>
2026-09-11 11:16:52 +0200

    uppgift 2.1

--- variabler.py
+++ variabler.py
@@ -1,4 +1,7 @@
 mätarinställning=int(input("mätarinställning idag?"))
 mätarinställning_för_ett_år_sedan=int(input("mätarinställning för ett år sedan?"))
-antal_mil=mätarinställning-mätarinställning_för_ett_år_sedan
-print(antal_mil)
+antal_mil_körda=mätarinställning-mätarinställning_för_ett_år_sedan
+print(antal_mil_körda)
+antal_liter_bensin=int(input("antal liter"))
+förbrukning_per_mil=antal_liter_bensin/antal_mil_körda
+print(förbrukning_per_mil)
